3207228bba6a7cbe50c86c4edc6b1f6e39da25d9
lukules <kulesza.lukasz@icloud.com>
2024-11-13 13:46:56 +0100

    stb_image added

--- sobel.cpp
+++ sobel.cpp
@@ -1,20 +1,37 @@
-#include "utility.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
 #include <omp.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 const int SOBEL_X[3][3] = {
     {-1, 0, 1},
     {-2, 0, 2},
-    {-1, 0, 1}}
-;
+    {-1, 0, 1}
+};
+
+const int SOBEL_Y[3][3] = {
+    {1, 2, 1},
+    {0, 0, 0},
+    {-1, -2, -1}
+};
 
 
+int main() {
+    
+    int width, height, channels;
+    unsigned char *image = stbi_load("../image.png", &width, &height, &channels, 0);
 
-int main(int argc, char** argv){
+    if (image == NULL) {
+        printf("Error loading image\n");
+        return 1;
+    }
 
-    printf("Hello from process: %d\n", omp_get_thread_num());
+    printf("Loaded image with width: %d, height: %d, channels: %d\n", width, height, channels);
 
+ 
+
+    stbi_image_free(image);
     return 0;
 }
